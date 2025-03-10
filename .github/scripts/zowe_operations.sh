3f17279d1e767da9b5c947d7d6b72cfcc07e5d42
oscarbustelo <39202422+oscarbustelo@users.noreply.github.com>
2025-03-10 17:29:52 -0300

    Update zowe_operations.sh
    ensalada

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -12,13 +12,16 @@ else
 fi
 # Upload files
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "bin/cobol-check-0.2.16.jar"
+#zowe zos-files upload file-to-uss cobol-check-0.2.16.jar "/z/z53746/cobolcheck/bin/cobol-check-0.2.16.jar" --binary
 
 #zowe zos-files upload dir-to-uss "./cobol-check/scripts" "/z/$LOWERCASE_USERNAME/cobolcheck/scripts" --recursive
-#zowe zos-files upload dir-to-uss "./cobol-check/src" "/z/$LOWERCASE_USERNAME/cobolcheck/src" --recursive
 zowe zos-files upload dir-to-uss ./cobol-check/scripts /z/z53746/cobolcheck/scripts --recursive --encoding UTF-8 --binary false
-
 #zowe zos-files upload dir-to-uss "./cobol-check/scripts" "/z/$LOWERCASE_USERNAME/cobolcheck/scripts" --recursive --text-files "*"
-zowe zos-files upload dir-to-uss "./cobol-check/src" "/z/$LOWERCASE_USERNAME/cobolcheck/src" --recursive --text-files "*"
+
+#zowe zos-files upload dir-to-uss "./cobol-check/src" "/z/$LOWERCASE_USERNAME/cobolcheck/src" --recursive
+#zowe zos-files upload dir-to-uss "./cobol-check/src" "/z/$LOWERCASE_USERNAME/cobolcheck/src" --recursive --text-files "*"
+zowe zos-files upload dir-to-uss "./cobol-check/src" "/z/$LOWERCASE_USERNAME/cobolcheck/src" --recursive --encoding UTF-8 --binary false
+
 
 # Verify upload
 echo "Verifying upload:"
