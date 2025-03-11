2521ebeb31121faa778dfd0473c507620ef3253a
oscarbustelo <39202422+oscarbustelo@users.noreply.github.com>
2025-03-11 17:12:26 -0300

    Update mainframe_operations.sh
    saque el jcl

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -41,27 +41,7 @@ run_cobolcheck() {
     else
         echo "CC##99.CBL not found for $program"
     fi            
-    #    if cp CC##99.CBL "//'${ZOWE_USERNAME}.CBL($program)'"; then
 
-    # Verificar si el archivo JCL existe
-    ##if [ -f "../temp/${program}.JCL" ]; then
-    ##    # Copiar el archivo JCL a la carpeta USS
-    ##    if cp "../temp/${program}.JCL" "/z/${ZOWE_USERNAME}.JCL(${program})"; then
-    ##        echo "Copied ${program}.JCL to /z/z53746/jcl/${program}.JCL"
-    ##    else
-    ##        echo "Failed to copy ${program}.JCL to /z/z53746/jcl/${program}.JCL"
-    ##    fi
-    ##else
-    ##    echo "${program}.JCL not found in ../temp"
-    ##fi
-
-
-    # Copy the JCL file if it exists
-    #if [ -f "${program}.JCL" ]; then
-    #    if cp ${program}.JCL "//'${ZOWE_USERNAME}.JCL($program)'"; then
-    #        echo "Copied ${program}.JCL to ${ZOWE_USERNAME}.JCL($program)"
-    #   else
-    #        echo "Failed to copy ${program}.JCL to ${ZOWE_USERNAME}.JCL($program)"
 
 }
 
